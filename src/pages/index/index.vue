28b6463ff09728d970b8027b0e3c113075bf2632
ice breaker <1324318532@qq.com>
2025-03-06 15:53:04 +0800

    chore(deps): upgrade

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="flex">
+  <view class="flex flex-col items-center justify-center space-y-4">
     <image class="w-20 h-20" src="/static/logo.png" />
     <view class="text-area">
       <text class="text-[#00f285] text-[88rpx] font-bold underline">{{ title }}</text>
@@ -7,13 +7,17 @@
     <view :class="className">
       {{ className }}
     </view>
+    <IceButton custom-class="bg-amber-600">
+      获取值
+    </IceButton>
   </view>
 </template>
 
 <script setup lang="ts">
+import IceButton from '@/components/IceButton.vue'
 import { ref } from 'vue'
 const title = ref('Hello')
-const className = ref('bg-[#0000ff]')
+const className = ref('bg-[#00aa55]')
 </script>
 
 
