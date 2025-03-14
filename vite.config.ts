2b918a102672b2eca9e09b7cdc5d1d1977a1f58d
ice breaker <1324318532@qq.com>
2025-03-15 00:22:29 +0800

    chore: add WeappTailwindcssDisabled

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
-// dynamic require of is not supported
-// const uni = require("@dcloudio/vite-plugin-uni");
+import process from 'node:process'
+
+const isH5 = process.env.UNI_PLATFORM === 'h5'
+const isApp = process.env.UNI_PLATFORM === 'app'
+const WeappTailwindcssDisabled = isH5 || isApp
 import uni from "@dcloudio/vite-plugin-uni";
 import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
 // import tailwindcss from '@tailwindcss/vite'
@@ -17,7 +20,8 @@ export default defineConfig(async () => {
       tailwindcss(),
       UnifiedViteWeappTailwindcssPlugin(
         {
-          rem2rpx: true
+          rem2rpx: true,
+          disabled: WeappTailwindcssDisabled
         }
       )
     ],
