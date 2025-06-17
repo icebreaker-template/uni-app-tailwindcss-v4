db52e4781cf227e34d60e23ddae7eccc3313d1a4
ice breaker <1324318532@qq.com>
2025-06-17 17:14:22 +0800

    chore: upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,23 @@ const WeappTailwindcssDisabled = isH5 || isApp
 import uni from "@dcloudio/vite-plugin-uni";
 import { UnifiedViteWeappTailwindcssPlugin } from 'weapp-tailwindcss/vite'
 import tailwindcss from '@tailwindcss/postcss'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from '@uni-helper/vite-plugin-uni-components'
+import { WotResolver } from '@uni-helper/vite-plugin-uni-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     // 改成 mts，则爆 uni is not a function
+    AutoImport({
+      imports: [
+        'vue',
+        'uni-app',
+      ],
+    }),
+    Components({
+      resolvers: [WotResolver()]
+    }),
     uni(),
     UnifiedViteWeappTailwindcssPlugin(
       {
@@ -23,7 +35,11 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [
-        tailwindcss()
+        tailwindcss(
+          {
+            base: __dirname
+          }
+        )
       ]
     }
   }
