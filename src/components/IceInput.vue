9b1d840f890ecd8ddf432e1d2ab8b4eff6cdec00
ice breaker <1324318532@qq.com>
2025-11-08 22:54:59 +0800

    chore: add case

---
+++ src/components/IceInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <view>
+        <input v-model="model"></input>
+    </view>
+</template>
+
+<script setup lang="ts">
+
+const model = defineModel()
+const weappTwIgnore = String.raw
+
+const world = {
+    Accept: `text/event-stream`,
+}
+console.log(world)
+</script>
+
+<style scoped></style>
