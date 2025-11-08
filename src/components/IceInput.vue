5441704f9570434d2942532900b6e3a359df6384
ice breaker <1324318532@qq.com>
2025-11-08 23:17:58 +0800

    chore: add weappTwIgnore

--- src/components/IceInput.vue
+++ src/components/IceInput.vue
@@ -11,6 +11,8 @@ const weappTwIgnore = String.raw
 
 const world = {
     Accept: `text/event-stream`,
+    Vccept: weappTwIgnore`text/stream`,
+    Cccept: `text`+`/stream`,
 }
 console.log(world)
 </script>
